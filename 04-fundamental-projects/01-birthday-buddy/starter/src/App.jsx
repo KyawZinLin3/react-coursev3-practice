fe1213a2fdb27e214fbe035779fcafa86e86bb35
KyawZinLin3 <kyawzinlin14future@gmail.com>
2024-02-12 23:03:20 +0630

    making birthday project

--- 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,35 @@
+import data from "../src/data";
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  var total = people.length;
+  //console.log(people);
+
+  const handelDeleteAll = () => {
+    setPeople([]);
+  };
+  return (
+    <>
+      <div className="container">
+        <h3>{total} Birthdays Today</h3>
+        {people.map((person) => {
+          const { id, name, age, image } = person;
+          return (
+            <>
+              <div className="person" key={id}>
+                <img src={image} alt={name} />
+                <h4>{name}</h4>
+                <p>{age} years</p>
+              </div>
+            </>
+          );
+        })}
+        <button type="button" onClick={handelDeleteAll} className="btn">
+          Delete All
+        </button>
+      </div>
+    </>
+  );
 };
 export default App;
