f4a6f70419d5a124fed60506a319e7d8aced1804
KyawZinLin3 <kyawzinlin14future@gmail.com>
2024-02-13 23:31:04 +0630

    learning

--- 04-fundamental-projects/02-tours/starter/src/App.jsx
+++ 04-fundamental-projects/02-tours/starter/src/App.jsx
@@ -1,6 +1,40 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useState, useEffect } from "react";
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    const fetchTours = async () => {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.log(tours);
+      setTours(tours);
+    };
+    fetchTours();
+  }, []);
+  return (
+    <main>
+      <section>
+        <div className="tours">
+          {tours.map((tour) => {
+            return (
+              <article className="single-tour" key={tour.id}>
+                <img src={tour.image} alt="" className="img" />
+                <span className="tour-price">{tour.price}</span>
+                <div className="tour-info">
+                  <h5>{tour.name}</h5>
+                  <p>{tour.info}</p>
+                  <button className="delete-btn btn-block btn">
+                    not interrested
+                  </button>
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    </main>
+  );
 };
 export default App;
